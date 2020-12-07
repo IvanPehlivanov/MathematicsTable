45c0fec2eaf68dbf44b700acaf8a969f1976c100
IvanPehlivanov <i.pehlivanov@yahoo.com>
2020-12-07 19:50:39 +0100

    All features are ready

---
+++ MathematicsTable/MathematicsTable/Calculations.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicsTable
+{
+    class Calculations
+    {
+        /// <summary>
+        /// calculate correct table values
+        /// </summary>
+        /// <param name="op">math function</param>
+        /// <returns>list with correct values</returns>
+        internal List<string[]> CalculateTable(string op)
+        {
+            List<string[]> correctValuesList = new List<string[]>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                string[] correctValues = new string[10];
+
+                for (int j = 0; j < 10; j++)
+                {
+                    if (op == "-")
+                    {
+                        double result = (i + 1.0) - (j + 1.0);
+                        correctValues[j] = result.ToString();
+                    }
+                    else if (op == "+")
+                    {
+                        double result = (i + 1.0) + (j + 1.0);
+                        correctValues[j] = result.ToString();
+                    }
+                    else if (op == "*")
+                    {
+                        double result = (i + 1.0) * (j + 1.0);
+                        correctValues[j] = result.ToString();
+                    }
+                    else if (op == "/")
+                    {
+                        double result = (i + 1.0) / (j + 1.0);
+                        result = Math.Round(result, 2);
+                        correctValues[j] = result.ToString();
+                    }
+                }
+                correctValuesList.Add(correctValues);
+            }
+
+            return correctValuesList;
+        }
+
+        /// <summary>
+        /// compares two array lists
+        /// </summary>
+        /// <param name="correctValuesList">first list</param>
+        /// <param name="userInputList">second list</param>
+        /// <returns>list of wrong line numbers</returns>
+        internal List<int> CompareLists(List<string[]> correctValuesList, List<string[]> userInputList)
+        {
+            bool isOk = false;
+            List<int> wrongLineNumber = new List<int>();
+
+            for (int i = 0; i < correctValuesList.Count; i++)
+            {
+                isOk = correctValuesList[i].SequenceEqual(userInputList[i]);
+                if (!isOk)
+                {
+                    wrongLineNumber.Add(i);
+                }
+            }
+
+            return (wrongLineNumber);
+        }
+    }
+}
---
+++ MathematicsTable/MathematicsTable/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MathematicsTable
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<string[]> ArrCorrectValues { get; set; }
+        public string[] Operator { get; set; }
+        public List<string[]> ArrUserInput { get; set; }
+
+        private readonly Calculations _calculations;
+
+        private string mathFunction;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            Operator = new string[] { "-", "+", "*", "/" };
+
+            _calculations = new Calculations();
+
+            ArrCorrectValues = new List<string[]>();
+            ArrCorrectValues.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrCorrectValues.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrCorrectValues.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrCorrectValues.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrCorrectValues.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrCorrectValues.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrCorrectValues.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrCorrectValues.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrCorrectValues.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrCorrectValues.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+
+
+            ArrUserInput = new List<string[]>();
+            ArrUserInput.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrUserInput.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrUserInput.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrUserInput.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrUserInput.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrUserInput.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrUserInput.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrUserInput.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrUserInput.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+            ArrUserInput.Add(new string[] { "0", "0", "0", " 0", "0", "0", "0", "0", "0", "0" });
+
+
+            DgUser.ItemsSource = ArrCorrectValues;
+            Combo.ItemsSource = Operator;
+        }
+
+        private void Combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            mathFunction = (sender as ComboBox).SelectedValue.ToString();
+            ArrCorrectValues = _calculations.CalculateTable(mathFunction);
+            myRandom();
+            DgUser.ItemsSource = null;
+            DgUser.ItemsSource = ArrCorrectValues;
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var Ok = MessageBox.Show("Are you want to close", "MathematicsTableZP", MessageBoxButton.YesNo,
+                MessageBoxImage.Question);
+            if (Ok == MessageBoxResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        public void myRandom()
+        {
+            Random ran = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                int pp = ran.Next(9);
+                int po = ran.Next(9);
+
+                ArrCorrectValues[pp][po] = string.Empty;
+            }
+        }
+
+        private void BtnCheckUserInput(object sender, RoutedEventArgs e)
+        {
+            ArrUserInput = DgUser.ItemsSource as List<string[]>;
+            var wrongLines = CheckUserInput(ArrUserInput, mathFunction);
+            if (wrongLines.Any())
+            {
+                MessageBox.Show($"Wrong value in{wrongLines.Count} line/s.");
+                ColorRow(DgUser, wrongLines);
+            }
+            else
+            {
+                MessageBox.Show("All values are correct.");
+            }
+        }
+
+        /// <summary>
+        /// check if user input is correct
+        /// </summary>
+        /// <param name="userInput">user input value</param>
+        /// <param name="correctValue">correct value</param>
+        /// <returns>true if user input is correct</returns>
+        internal List<int> CheckUserInput(List<string[]> userInput, string mathOp)
+        {
+            var correctValuesList = _calculations.CalculateTable(mathOp);
+            return _calculations.CompareLists(correctValuesList, userInput);
+
+        }
+
+        private void ColorRow(DataGrid dg, List<int> linesToMark)
+        {
+            for (int i = 0; i < dg.Items.Count; i++)
+            {
+                //DataGridRow row = (DataGridRow) dg.ItemContainerGenerator.ContainerFromIndex(i);
+
+                //    if (row != null)
+                //    {
+                //        int index = row.GetIndex();
+                //        if (index % 2 == 0)
+                //        {
+                //            SolidColorBrush brush = new SolidColorBrush(Color.FromArgb(100, 255, 104, 0));
+                //            row.Background = brush;
+                //        }
+                //        else
+                //        {
+                //            SolidColorBrush brush = new SolidColorBrush(Color.FromArgb(100, 255, 232, 0));
+                //            row.Background = brush;
+                //        }
+                //    }
+                //}
+
+                foreach (var rowNumber in linesToMark)
+                {
+                    DataGridRow row = (DataGridRow)dg.ItemContainerGenerator.ContainerFromIndex(rowNumber);
+                    if (row != null)
+                    {
+                        SolidColorBrush brush = new SolidColorBrush(Color.FromArgb(100, 255, 104, 0));
+                        row.Background = brush;
+                    }
+                }
+            }
+        }
+    }
+}
